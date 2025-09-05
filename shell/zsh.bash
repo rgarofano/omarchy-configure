b0d993eab7449c6fb05cd0a2ed4be5195504aa45
Ryan Garofano <ryan@ryangarofano.ca>
2025-09-04 21:35:07 -0700

    Add space to >>

--- shell/zsh.bash
+++ shell/zsh.bash
@@ -9,7 +9,7 @@ sudo chsh -s "$ZSH_PATH" "$USER"
 echo "Done"
 
 echo "Switching shell to $ZSH_PATH in alacritty config"
-echo -e "\n[terminal]\n\n[terminal.shell]\nprogram = \"$ZSH_PATH\"" >>"$HOME/.config/alacritty/alacritty.toml"
+echo -e "\n[terminal]\n\n[terminal.shell]\nprogram = \"$ZSH_PATH\"" >> "$HOME/.config/alacritty/alacritty.toml"
 echo "Done"
 
 echo "Setting up zshrc"
