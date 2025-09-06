3a98a994bdfc59a3d7114de548c6ceed58ada774
Ryan Garofano <ryan@ryangarofano.ca>
2025-09-06 12:27:28 -0700

    Add lsp installation

--- dev/lua.bash
+++
@@ -1,1 +0,0 @@
-sudo pacman -Sy --noconfirm lua-language-server
---
+++ editor/lsp.bash
@@ -0,0 +1,3 @@
+sudo pacman -Sy --noconfirm lua-language-server
+sudo pacman -Sy --noconfirm typescript-language-server
+sudo pacman -Sy --noconfirm tailwindcss-language-server
